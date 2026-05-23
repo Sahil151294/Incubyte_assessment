2cdf65ff949c5b1c5a3891c043cc472d56f5675a
Sahil Sengupta <iamsahilsengupta@gmail.com>
2026-05-23 16:39:15 +0530

    removing redundant data

--- tests/loginTest.spec.ts
+++ tests/loginTest.spec.ts
@@ -6,12 +6,6 @@ const surname:string="Admin"
 const username:string="PolarBear15"   // Do CHANGE the username everytime you run testcases
 const password:string="Bazinga"
 const url = 'https://parabank.parasoft.com/parabank/index.htm?ConnType=JDBC'
-const testData = [
-  { username: 'user111', password:'', name:'Polar', surname:'Admin'},// checking login FAILS if null password is sent to login
-  { username: '', password:'password1', name:'PolarBear', surname:'Admin'},// checking login FAILS if null username is sent to login
-  { username: 'user211', password:'password1', name:'', surname:'Admin'},// checking login FAILS if null name is sent to customer registration
-  { username: 'user311', password:'password1', name:'NewPolarBear', surname:''}// checking login FAILS if null surname is sent to customer registration
-];
 
 test.fail('Unsuccessful login with unregistered credentials', async ({ page }) => {
   const originPag = new LoginPage(page, url);
